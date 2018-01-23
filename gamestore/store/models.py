c7abdde7a95f2c70615907756c9474991f4de85d
Max Estlander <max.estlander@aalto.fi>
2018-01-23 13:27:22 +0200

    added __str__ functions

--- gamestore/store/models.py
+++ gamestore/store/models.py
@@ -5,10 +5,16 @@ class Category(models.Model):
     id = models.AutoField(primary_key = True)
     name = models.CharField(max_length = 30, unique = True)
 
+    def __str__(self):
+        return self.name
+
 
 class Game(models.Model):
     id = models.AutoField(primary_key = True)
     name = models.CharField(max_length = 50, unique = True)
     category = models.ForeignKey(Category, null = False, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
