46ed5ee3ac229bce972f8171f16f21ae704eae59
Max Estlander <max.estlander@aalto.fi>
2018-01-23 13:31:45 +0200

    Pylinted

--- gamestore/store/models.py
+++ gamestore/store/models.py
@@ -1,18 +1,22 @@
+"""Barebones structure for models currently has name and id only
+TODO: add in rest of required fields"""
 from django.db import models
-#Basic models for the store currentlyonly has id for primary key purposes and name
+
 
 class Category(models.Model):
-    id = models.AutoField(primary_key = True)
-    name = models.CharField(max_length = 30, unique = True)
+    """Category of games skeleton"""
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30, unique=True)
 
     def __str__(self):
         return self.name
 
 
 class Game(models.Model):
-    id = models.AutoField(primary_key = True)
-    name = models.CharField(max_length = 50, unique = True)
-    category = models.ForeignKey(Category, null = False, on_delete = models.CASCADE)
+    """Games skeleton structure"""
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50, unique=True)
+    category = models.ForeignKey(Category, null=False, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
