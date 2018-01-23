74b97b667f3240199f66d000b71d9051175f20c3
Lauri <lauri.a.koskela@aalto.fi>
2018-01-23 13:58:43 +0200

    Added logging in support

--- gamestore/store/views.py
+++ gamestore/store/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required, permission_required
 
+@login_required()
 def index(request):
     return HttpResponse("Hello, world. You're at the store index.")
