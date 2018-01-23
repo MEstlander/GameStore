ae34beb6e3f404770eb95a85af6de44f824991d8
Max Estlander <max.estlander@aalto.fi>
2018-01-23 13:11:47 +0200

    Added structure for models game and category

--- gamestore/store/models.py
+++ gamestore/store/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+#Basic models for the store currentlyonly has id for primary key purposes and name
+
+class Category(models.Model):
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 30, unique = True)
+
+
+class Game(models.Model):
+    id = models.AutoField(primary_key = True)
+    name = models.CharField(max_length = 50, unique = True)
+    category = models.ForeignKey(Category, null = False, on_delete = 'CASCADE')
 
 # Create your models here.
