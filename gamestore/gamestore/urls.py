926a1cf98e751915eb5b997cf6c3cc2fe1520ba7
rasmusblassar <rasmus.blassar@gmail.com>
2018-01-23 11:40:51 +0200

    reinitialize django

--- gamestore/gamestore/urls.py
+++ gamestore/gamestore/urls.py
@@ -1,4 +1,4 @@
-"""website URL Configuration
+"""gamestore URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
