33270f870039057277b11783774ecc863b5082ed
Lauri <lauri.a.koskela@aalto.fi>
2018-01-30 11:14:23 +0200

    Added gaming app and fixed models

--- gamestore/users/models.py
+++ gamestore/users/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class  Profile(models.Model):
-    user = models.OneToOneField(User)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
     owned_games = models.ManyToManyField('Store.Game',default=None,blank=True)
 
     #return username when the Profile is called
