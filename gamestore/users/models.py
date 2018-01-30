300e052e6b9e09029073ceb5e1bfad195ad0331e
Max Estlander <max.estlander@aalto.fi>
2018-01-30 10:52:21 +0200

    added str method and onetoonefield in django

--- gamestore/users/models.py
+++ gamestore/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class  Profile(models.Model):
+    user = models.OneToOneField(User)
+    
+
+    #return username when the Profile is called
+    def __str__(self):
+        return str(self.user.username)
