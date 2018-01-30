093b7c12971540ca16dcfcd376af6cb0e0bb549d
Lauri <lauri.a.koskela@aalto.fi>
2018-01-30 12:47:11 +0200

    Added iframe support

--- gamestore/gaming/views.py
+++ gamestore/gaming/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required, permission_required
+from store.models import Game
+from django.shortcuts import get_object_or_404
 # Create your views here.
+
+@login_required
+def play(request,id):
+    game = get_object_or_404(Game, id=id)
+    
+    return render(request,"play_game/play.html", {'remote_server': game.url})
