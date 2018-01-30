c3011c3a611bb7ba6d437e04f52345eafa526c64
Lauri <lauri.a.koskela@aalto.fi>
2018-01-30 10:58:25 +0200

    Added owned_games to User

--- gamestore/users/models.py
+++ gamestore/users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class  Profile(models.Model):
     user = models.OneToOneField(User)
-    
+    owned_games = models.ManyToManyField('Store.Game',default=None,blank=True)
 
     #return username when the Profile is called
     def __str__(self):
