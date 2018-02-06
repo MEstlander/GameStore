8edbe376927b8bd8396cef78e6515d75898af11f
Lauri <lauri.a.koskela@aalto.fi>
2018-02-06 11:38:12 +0200

    Added post-save hooks to create proifle and save profile

--- gamestore/users/models.py
+++ gamestore/users/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-import store.models as x
+from djano.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
-class  Profile(models.Model):
+class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
     owned_games = models.ManyToManyField(x.Game,default=None,blank=True)
     is_developer = models.BooleanField(default=False) #player if false, developer if true
@@ -10,4 +11,12 @@ class  Profile(models.Model):
     #return username when the Profile is called
     def __str__(self):
         return str(self.user.username)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)          
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
     
