db083ed6bad154ba115b3bbf971c93d7997eae8d
Lauri <lauri.a.koskela@aalto.fi>
2018-02-26 22:06:59 +0200

    plz fix this

--- gamestore/gamestore/wsgi.py
+++ gamestore/gamestore/wsgi.py
@@ -7,11 +7,8 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 """
 
-import os
-
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamestore.settings")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
