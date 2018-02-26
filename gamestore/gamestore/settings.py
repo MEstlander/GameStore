7189acb754b46ec1ed2d5ef8d4a59b6627872d10
Lauri <lauri.a.koskela@aalto.fi>
2018-02-26 23:21:14 +0200

    Dangerous migrations

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -151,9 +151,8 @@ STATICFILES_FINDERS = [
 
 YARN_ROOT_PATH = os.path.join(BASE_DIR,'..')
 
-MEDIA_ROOT = (
-    os.path.join(BASE_DIR, 'media')
-)
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 MEDIA_URL = '/media/'
 
