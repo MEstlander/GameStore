d9af0c3f12840efaa9d70ab348fceb6f6d048969
Lauri <lauri.a.koskela@aalto.fi>
2018-02-26 14:20:32 +0200

    Modified settings.py

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -78,12 +78,16 @@ WSGI_APPLICATION = 'gamestore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+if 'DATABASE_URL' in os.environ:
+    import dj_database_url
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
@@ -153,7 +157,3 @@ MEDIA_ROOT = (
 
 MEDIA_URL = '/media/'
 
-
-if 'DATABASE_URL' in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
