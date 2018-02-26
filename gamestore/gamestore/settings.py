43429d08b0cd7c61e21787a56898813229e901d8
Lauri <lauri.a.koskela@aalto.fi>
2018-02-26 22:02:55 +0200

    Fixing static error by adding whitenoise to wsgi.py

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3wmp5u3e#4vso-&xqk42c)po@mc+0kggjfzfn@gyo^gp6!_m5t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['nameless-retreat-59006.herokuapp.com', 'localhost', '127.0.0.1']
 
