1327b3882c0d17718741946cf30e96c3123a6ca5
rasmusblassar <rasmus.blassar@gmail.com>
2018-03-03 21:19:14 +0200

    Change back to sqlite3

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -80,12 +80,8 @@ if "DYNO" in os.environ:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'gamestore',
-            'USER': 'blassar1',
-            'PASSWORD': 'password',
-            'HOST': 'localhost',
-            'PORT': '',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 
