e7beb9cdc55045f54f95b26bf053bc7af86de16b
rasmusblassar <rasmus.blassar@gmail.com>
2018-03-03 19:48:52 +0200

    Change allowed_hosts

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'Change me'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://lit-temple-88594.herokuapp.com/']
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
