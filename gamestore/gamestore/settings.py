93e3bb4dcbe4806f477481f11e27ce21f5be3d99
rasmusblassar <rasmus.blassar@gmail.com>
2018-03-03 21:05:17 +0200

    Add local postgres db

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -80,10 +80,15 @@ if "DYNO" in os.environ:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'gamestore',
+            'USER': 'blassar1',
+            'PASSWORD': 'password',
+            'HOST': 'localhost',
+            'PORT': '',
         }
     }
+
 
 
 # Password validation
