05fa42f87c1f3af2ead5d9cb1f64626ff28c7cc8
rasmusblassar <rasmus.blassar@gmail.com>
2018-03-03 20:48:42 +0200

    Try deploy with sqlite

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -74,16 +74,13 @@ WSGI_APPLICATION = 'gamestore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-if "DYNO" in os.environ:
-    import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+}
 
 
 # Password validation
