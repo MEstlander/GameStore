e44c68ef15b5cac48cc5e868eaa042d076dd1423
rasmusblassar <rasmus.blassar@gmail.com>
2018-03-03 21:15:06 +0200

    Modify settings

--- gamestore/gamestore/settings.py
+++ gamestore/gamestore/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'gamestore.wsgi.application'
 
 if "DYNO" in os.environ:
     import dj_database_url
-    DATABASES = {'default': dj_database_url.config()}
+    DATABASES = {'default': dj_database_url.config(conn_max_age=600, ssl_require=True)}
 else:
     DATABASES = {
         'default': {
